783f52807c656476e31e1d9a1fc424ce2a7259ae
Limbani <lchaponda@umass.edu>
2026-02-11 19:01:09 -0500

    express server running and docker scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Welcome to the Currency Converter!");
+import express from "express";
+
+const app = express(); 
+const PORT = 3000; 
+
+app.listen(PORT, () => { 
+    console.log(`Welcome to Currency-Converter! Server is running on PORT:${PORT}`)
+}); 
+
+
