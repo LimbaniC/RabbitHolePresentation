9a274286eafa9106e7aee51f66770c66bc2e670a
Limbani <lchaponda@umass.edu>
2026-02-14 16:22:19 -0500

    currency-api hello world

---
+++ currency-api/dist/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Welcome to Currency-API! Server is running on PORT:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
